4db9923b2587935c8e3c8e25d201e6be895078bf
069464341 <theara.watt@gmail.com>
2016-03-16 14:45:43 +0700

    new version

---
+++ export_import/backup2/export.sh
@@ -0,0 +1,75 @@
+mongoexport --db meteor --collection answerquizz --out answerquizz.json
+wait
+mongoexport --db meteor --collection attribute --out attribute.json
+wait
+mongoexport --db meteor --collection attribute_value --out attribute_value.json
+wait
+mongoexport --db meteor --collection banner --out banner.json
+wait
+mongoexport --db meteor --collection card --out card.json
+wait
+mongoexport --db meteor --collection cart --out cart.json
+wait
+mongoexport --db meteor --collection categories --out categories.json
+wait
+mongoexport --db meteor --collection cfs.images.filerecord --out cfs.images.filerecord.json
+wait
+mongoexport --db meteor --collection cfs._tempstore --out cfs._tempstore.json
+wait
+mongoexport --db meteor --collection contents --out contents.json
+wait
+mongoexport --db meteor --collection contents_type --out contents_type.json
+wait
+mongoexport --db meteor --collection daily --out daily.json
+wait
+mongoexport --db meteor --collection discount --out discount.json
+wait
+mongoexport --db meteor --collection favorite --out favorite.json
+wait
+mongoexport --db meteor --collection journey --out journey.json
+wait
+mongoexport --db meteor --collection linkselling --out linkselling.json
+wait
+mongoexport --db meteor --collection list_product --out list_product.json
+wait
+mongoexport --db meteor --collection meteor_accounts_loginServiceConfiguration --out meteor_accounts_loginServiceConfiguration.json
+wait
+mongoexport --db meteor --collection meteor_oauth_pendingCredentials --out meteor_oauth_pendingCredentials.json
+wait
+mongoexport --db meteor --collection meteor_oauth_pendingRequestTokens --out meteor_oauth_pendingRequestTokens.json
+wait
+mongoexport --db meteor --collection mouse --out mouse.json
+wait
+mongoexport --db meteor --collection order --out order.json
+wait
+mongoexport --db meteor --collection p2 --out p2.json
+wait
+mongoexport --db meteor --collection parentattr --out parentattr.json
+wait
+mongoexport --db meteor --collection parent_tags --out parent_tags.json
+wait
+mongoexport --db meteor --collection payment --out payment.json
+wait
+mongoexport --db meteor --collection posts --out posts.json
+wait
+mongoexport --db meteor --collection products --out products.json
+wait
+mongoexport --db meteor --collection question --out question.json
+wait
+mongoexport --db meteor --collection quizz --out quizz.json
+wait
+mongoexport --db meteor --collection roles --out roles.json
+wait
+mongoexport --db meteor --collection shops --out shops.json
+wait
+mongoexport --db meteor --collection stock --out stock.json
+wait
+mongoexport --db meteor --collection tags --out tags.json
+wait
+mongoexport --db meteor --collection tracking --out tracking.json
+wait
+mongoexport --db meteor --collection translation --out translation.json
+wait
+mongoexport --db meteor --collection users --out users.json
+wait
+mongoexport --db meteor --collection userTracking --out userTracking.json
---
+++ export_import/launcher.sh
@@ -0,0 +1,75 @@
+mongoimport --db meteor --collection answerquizz --drop --file answerquizz.json --jsonArray
+wait
+mongoimport --db meteor --collection attribute --drop --file attribute.json --jsonArray
+wait
+mongoimport --db meteor --collection attribute_value --drop --file attribute_value.json --jsonArray
+wait
+mongoimport --db meteor --collection banner --drop --file banner.json --jsonArray
+wait
+mongoimport --db meteor --collection card --drop --file card.json --jsonArray
+wait
+mongoimport --db meteor --collection cart --drop --file cart.json --jsonArray
+wait
+mongoimport --db meteor --collection categories --drop --file categories.json --jsonArray
+wait
+mongoimport --db meteor --collection cfs.images.filerecord --drop --file cfs.images.filerecord.json --jsonArray
+wait
+mongoimport --db meteor --collection cfs._tempstore --drop --file cfs._tempstore.json --jsonArray
+wait
+mongoimport --db meteor --collection contents --drop --file contents.json --jsonArray
+wait
+mongoimport --db meteor --collection contents_type --drop --file contents_type.json --jsonArray
+wait
+mongoimport --db meteor --collection daily --drop --file daily.json --jsonArray
+wait
+mongoimport --db meteor --collection discount --drop --file discount.json --jsonArray
+wait
+mongoimport --db meteor --collection favorite --drop --file favorite.json --jsonArray
+wait
+mongoimport --db meteor --collection journey --drop --file journey.json --jsonArray
+wait
+mongoimport --db meteor --collection linkselling --drop --file linkselling.json --jsonArray
+wait
+mongoimport --db meteor --collection list_product --drop --file list_product.json --jsonArray
+wait
+mongoimport --db meteor --collection meteor_accounts_loginServiceConfiguration --drop --file meteor_accounts_loginServiceConfiguration.json --jsonArray
+wait
+mongoimport --db meteor --collection meteor_oauth_pendingCredentials --drop --file meteor_oauth_pendingCredentials.json --jsonArray
+wait
+mongoimport --db meteor --collection meteor_oauth_pendingRequestTokens --drop --file meteor_oauth_pendingRequestTokens.json --jsonArray
+wait
+mongoimport --db meteor --collection mouse --drop --file mouse.json --jsonArray
+wait
+mongoimport --db meteor --collection order --drop --file order.json --jsonArray
+wait
+mongoimport --db meteor --collection p2 --drop --file p2.json --jsonArray
+wait
+mongoimport --db meteor --collection parent_tags --drop --file parent_tags.json --jsonArray
+wait
+mongoimport --db meteor --collection parentattr --drop --file parentattr.json --jsonArray
+wait
+mongoimport --db meteor --collection payment --drop --file payment.json --jsonArray
+wait
+mongoimport --db meteor --collection posts --drop --file posts.json --jsonArray
+wait
+mongoimport --db meteor --collection products --drop --file products.json --jsonArray
+wait
+mongoimport --db meteor --collection question --drop --file question.json --jsonArray
+wait
+mongoimport --db meteor --collection quizz --drop --file quizz.json --jsonArray
+wait
+mongoimport --db meteor --collection shops --drop --file shops.json --jsonArray
+wait
+mongoimport --db meteor --collection stock --drop --file stock.json --jsonArray
+wait
+mongoimport --db meteor --collection roles --drop --file roles.json --jsonArray
+wait
+mongoimport --db meteor --collection tags --drop --file tags.json --jsonArray
+wait
+mongoimport --db meteor --collection tracking --drop --file tracking.json --jsonArray
+wait
+mongoimport --db meteor --collection translation --drop --file translation.json --jsonArray
+wait
+mongoimport --db meteor --collection users --drop --file users.json --jsonArray
+wait
+mongoimport --db meteor --collection userTracking --drop --file userTracking.json --jsonArray
